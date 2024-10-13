26588185b39c8fa78268fbb8b89959852117a0ff
COCO <jesussanjuan23@gmail.com>
2024-10-12 21:30:57 -0500

    corregito

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -39,7 +39,8 @@ export const AuthProvider = ({ children }) => {
     });
 
     return () => unsubscribe();
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
 
   const logout = async () => {
     try {
--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore, query, updateDoc, where } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDycaioH9Wv9XnBszoXFVgsTUJSeWORV5I",
-  authDomain: "proyectf-21b95.firebaseapp.com",
-  projectId: "proyectf-21b95",
-  storageBucket: "proyectf-21b95.appspot.com",
-  messagingSenderId: "354072610928",
-  appId: "1:354072610928:web:b520df2055db6592310cb5"
+  apiKey: "AIzaSyAUt4q6sv-H64MZrQbBpIRAcB-DQT4ogV4",
+  authDomain: "proyecto-final-a2dd3.firebaseapp.com",
+  projectId: "proyecto-final-a2dd3",
+  storageBucket: "proyecto-final-a2dd3.appspot.com",
+  messagingSenderId: "419114290891",
+  appId: "1:419114290891:web:805a4c6db2542dce04d54e"
 };
 
 const app = initializeApp(firebaseConfig);
